12ea75c9490484abfe86ae0535c21150e981ee5d
wowbob396 <wowbob396@gmail.com>
2017-07-22 21:52:53 -0500

    updated homework

--- Homework/src/whackamole/WhackAMole.java
+++ Homework/src/whackamole/WhackAMole.java
@@ -3,13 +3,25 @@ package whackamole;
 import java.util.*;
 
 public class WhackAMole {
+	private static int gameAttempts;
+	private static int score;
+	private static int attemptsLeft;
+	private static int molesLeft;
+	private static int gameSize;
+	private static int[][] grid = new int[gameSize][gameSize];
 	
-	WhackAMole(int numAttempts,int gridDimension){
-		
+	public WhackAMole(int numAttempts,int gridDimension){
+		for(int i = 0; i < gameSize;i++){
+			for(int j = 0;j < gameSize; j++){
+				
+			}			
+		}
 	}
 	
 	public static boolean place(int x, int y){
-		return false;
+		boolean isMole = false;	
+		
+		return isMole;
 	}
 	
 	public static void whack(int x, int y){
